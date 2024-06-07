00575cd6be0bf058b0985c7653bef9e5a4b919f3
kuterok3r <slv00@vk.com>
2024-06-07 00:46:35 -0400

    Updated some files, created __build

---
+++ _build/CMakeFiles/RK2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RK2.dir/code/city.cpp.o"
+  "CMakeFiles/RK2.dir/code/city.cpp.o.d"
+  "CMakeFiles/RK2.dir/code/house.cpp.o"
+  "CMakeFiles/RK2.dir/code/house.cpp.o.d"
+  "CMakeFiles/RK2.dir/code/industry.cpp.o"
+  "CMakeFiles/RK2.dir/code/industry.cpp.o.d"
+  "CMakeFiles/RK2.dir/code/main.cpp.o"
+  "CMakeFiles/RK2.dir/code/main.cpp.o.d"
+  "CMakeFiles/RK2.dir/code/xmlvisitor.cpp.o"
+  "CMakeFiles/RK2.dir/code/xmlvisitor.cpp.o.d"
+  "RK2"
+  "RK2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RK2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _build/CMakeFiles/rk2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rk2.dir/code/city.cpp.o"
+  "CMakeFiles/rk2.dir/code/city.cpp.o.d"
+  "CMakeFiles/rk2.dir/code/house.cpp.o"
+  "CMakeFiles/rk2.dir/code/house.cpp.o.d"
+  "CMakeFiles/rk2.dir/code/industry.cpp.o"
+  "CMakeFiles/rk2.dir/code/industry.cpp.o.d"
+  "CMakeFiles/rk2.dir/code/main.cpp.o"
+  "CMakeFiles/rk2.dir/code/main.cpp.o.d"
+  "CMakeFiles/rk2.dir/code/xmlvisitor.cpp.o"
+  "CMakeFiles/rk2.dir/code/xmlvisitor.cpp.o.d"
+  "rk2"
+  "rk2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rk2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
